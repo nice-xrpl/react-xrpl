504ec1a735e80903d5e24a539b8858ff166c0b82
Akshay Dhalwala <akshay@trine.la>
2023-03-28 16:41:39 -0700

    package settings updates

---
+++ src/react-xrpl/index.ts
@@ -0,0 +1,12 @@
+export * from './constants';
+export * from './client';
+export * from './wallet';
+export * from './wallet-address';
+
+export * from './hooks';
+export * from './hooks/requests';
+export * from './hooks/transactions';
+
+export * from './api';
+export * from './api/requests';
+export * from './api/transactions';
--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/react-xrpl/index.ts'),
       name: "ReactXRPL",
       formats: ['es', 'umd'],
       fileName: 'index'
