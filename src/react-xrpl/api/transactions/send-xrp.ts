1d00be437e053ed107106b3727316a020edb59e1
Akshay Dhalwala <akshay@trine.la>
2023-03-27 19:48:59 -0700

    send-xrp now checks for invalid destination address

--- src/react-xrpl/api/transactions/send-xrp.ts
+++ src/react-xrpl/api/transactions/send-xrp.ts
@@ -1,7 +1,11 @@
-import { Client as xrplClient, Wallet as xrplWallet, xrpToDrops } from 'xrpl';
+import { Client as xrplClient, isValidAddress, Wallet as xrplWallet, xrpToDrops } from 'xrpl';
 
 export async function sendXRP(client: xrplClient, wallet: xrplWallet, destinationAddress: string, amount: number) {
 	await client.connect();
+
+	if (!isValidAddress(destinationAddress)) {
+		return Promise.reject('Invalid address');
+	}
 	
 	const tx = await client.submitAndWait({
 		TransactionType: 'Payment',
