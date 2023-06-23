c8b280ed665d9738be2daaf54491c73a7b04b5c5
Akshay Dhalwala <akshay@trine.la>
2023-06-23 15:00:43 -0700

    added vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), dts({
+  plugins: [tsconfigPaths(), nodePolyfills(), dts({
       include: ['src/']
     }), react()],
   define: {
